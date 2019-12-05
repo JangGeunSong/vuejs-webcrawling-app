c75d9a1d68cde16b7e59fb7be94d3e4ee9f42ffa
JangGeunSong <sjg1468@gmail.com>
2019-12-05 17:10:12 +0900

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container mx-auto px-4 py-4" id="app">
+    <Header />
+    <Contents msg="This is props sending test" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
+import Contents from './components/Contents.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Header,
+    Contents,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Contents.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="container">
+        Hello. This is container contents test boundary!<br />
+        {{ msg }}<br />
+        <button class="bg-red-600 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({
+    props: {
+        msg: String,
+    }
+})
+export default class Contents extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
