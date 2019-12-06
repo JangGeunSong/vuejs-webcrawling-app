319207a54960e5701e7bda4ddfe4a4279007f027
JangGeunSong <sjg1468@gmail.com>
2019-12-06 23:42:14 +0900

    Add fetching data for crawling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container mx-auto px-4 py-4" id="app">
     <Header />
-    <Contents msg="This is props sending test" />
+    <Contents msg="This is props sending test" v-bind:weather="weather"/>
+    <!-- 현재 props에서 자식 컴포넌트에 props를 보내기 위해 v-bind를 사용 -->
   </div>
 </template>
 
@@ -9,14 +10,27 @@
 import { Component, Vue } from 'vue-property-decorator';
 import Header from './components/Header.vue';
 import Contents from './components/Contents.vue';
+// react.js 처럼 컴포넌트들을 받아온다
+
+let weatherData = fetch('https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=광진구날씨')
+// props에 정의될 데이터를 미리 받아오는 활동은 스크립트 안에서 실행한다.
 
 @Component({
+  // 이 컴포넌트에서 렌더링할 컴포넌트들을 components:{} 안에 집어 넣는다 그럼 templates 태그 안에서 사용가능하다.
   components: {
     Header,
     Contents,
   },
+  // props의 정의를 할때 부모로 부터 받아와서 사용할 props는 props:{}를 통해 정의하고 직접 만들어 props로 쓸 것들은 data 함수로 받아온다
+  data: () => {
+    return {
+      weather: 'test hellos'
+    }
+  }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  
+}
 </script>
 
 <style>
--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -1,10 +1,11 @@
 <template>
     <div id="container">
-        Hello. This is container contents test boundary!<br />
-        {{ msg }}<br />
+        <p class="textColor-red font-bold">Hello. This is container contents test boundary!</p>
         <button class="bg-red-600 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
             Button
         </button>
+        <p class="textColor-blue font-bold p-3 m-3">{{ msg }}</p>
+        <p class="textColor-blue font-bold p-3 m-3">{{ weather }}</p>
     </div>
 
 </template>
@@ -15,6 +16,7 @@ import { Component, Vue, Prop } from 'vue-property-decorator'
 @Component({
     props: {
         msg: String,
+        weather: String,
     }
 })
 export default class Contents extends Vue {
